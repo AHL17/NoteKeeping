6649db4873686c475dcdc1415920093809f288be
xoozter <abigailhelenalang@gmail.com>
2022-12-10 14:46:15 +0700

    added create method of CRUD

--- app/controllers/notes_controller.rb
+++ app/controllers/notes_controller.rb
@@ -6,4 +6,18 @@ class NotesController < ApplicationController
   def show
     @note = Note.find(params[:id])
   end
+
+  def new
+    @note = Note.new
+  end
+
+  def create
+    @note = Note.new(title:"...", body:"...")
+
+    if @note.save
+      redirect_to @note
+    else
+      render :new, status: unprocessable_entity
+    end
+  end
 end
