cc04fe4701d5764efc7396546899d94ee7203ebf
xoozter <abigailhelenalang@gmail.com>
2022-12-10 14:34:08 +0700

    added routing control through index

--- app/controllers/notes_controller.rb
+++ app/controllers/notes_controller.rb
@@ -1,5 +1,9 @@
 class NotesController < ApplicationController
   def index
-    
+    @notes = Note.all
+  end
+
+  def show
+    @note = Note.find(params[:id])
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   root "notes#index"
-  
+
   get "/notes", to: "notes#index"
+  get "/notes/:id", to: "notes#show"
+ 
 end
