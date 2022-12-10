11a340484d5000b2d263f559deb39e4a7f6441b1
xoozter <abigailhelenalang@gmail.com>
2022-12-10 15:17:07 +0700

    added update method from CRUD

--- app/controllers/notes_controller.rb
+++ app/controllers/notes_controller.rb
@@ -21,6 +21,20 @@ class NotesController < ApplicationController
     end
   end
 
+  def edit
+    @note = Note.find(params[:id])
+  end
+
+  def update
+    @note = Note.find(params[:id])
+
+    if @note.update(note_params)
+      redirect_to @note
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
   private
     def note_params
       params.require(:note).permit(:title, :body)
