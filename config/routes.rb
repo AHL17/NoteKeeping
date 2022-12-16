90729b34de769cda86a50fd528c8f6cdd63dfc05
xoozter <abigailhelenalang@gmail.com>
2022-12-16 17:41:15 +0700

    added sign up feature, now user able to login to access note

--- config/routes.rb
+++ config/routes.rb
@@ -8,7 +8,8 @@ Rails.application.routes.draw do
   get 'login', to: 'sessions#new', as: 'login'
   get 'logout', to: 'sessions#destroy', as: 'logout'
 
-  resources :users
   resources :sessions
+  resources :users
+ 
 
 end
