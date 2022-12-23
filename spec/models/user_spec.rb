40dabe4d7ce19faa7eac166282987aff1ef5feb2
xoozter <abigailhelenalang@gmail.com>
2022-12-23 14:23:59 +0700

    added unit test for user model

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,21 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  context 'validation test' do
+    it 'ensures e-mail presence'do
+      user = User.new(password_digest: "").save
+      expect(user).to eq(false)
+
+    end
+    
+    it 'ensures password presence' do
+      user = User.new(email: "testing@yahoo.com").save
+      expect(user).to eq(false)
+    end
+
+    it 'should saved succesfully'do
+      user = User.new(email: "testing@yahoo.com", password_digest: "").save
+      expect(user).to eq(true)
+    end
+  end
 end
